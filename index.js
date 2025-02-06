f9e3a708a1a9bd7e5ef5c47a3fb147cd6b128825
Akanbi Omolade <omoladegraham@gmail.com>
2025-02-06 11:47:48 +0100

    recommited

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ if (close) {
     nav.classList.remove("active");
   });
 }
+
+// HEADER CHANGE BACKGROUND FROM SECTION TO SECTION
+window.addEventListener("scroll", () => {
+  var header = document.getElementById("header");
+  var hero = document.getElementById("hero");
+  if (hero.getBoundingClientRect().top <= 0) {
+    header.style.backgroundColor = "#000";
+  } else [(header.style.backgroundColor = "transparent")];
+});
